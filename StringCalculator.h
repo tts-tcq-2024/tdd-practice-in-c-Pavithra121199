d221f0b3a15903083e28be4d131b400fbd7b5a43
Pavithra121199 <65826287+Pavithra121199@users.noreply.github.com>
2024-07-25 17:21:40 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -8,49 +8,45 @@ int is_empty_string(const char* input) {
 }
 
 void extract_custom_delimiter(const char* input, char* delimiter) {
-    int i = 2;     // Start after the initial //
+    int i = 2;           // Start after the initial delimiter //
     delimiter[0] = '\0';
     while (input[i] != '\0' && input[i] != '\n') {
         strncat(delimiter, &input[i], 1);
         i++;
     }
 }
+
 void has_custom_delimiter(const char* input, char* delimiter) {
     if (input[0] == '/' && input[1] == '/')
         extract_custom_delimiter(input, delimiter);
     else
-         strcpy(delimiter, ",\n");
+        strcpy(delimiter, ",\n");
 }
 
-
-int value_less_than_thousand(const char *input_segment) {
-    int value_check = atoi(input_segment);
-    return (value_check < 1000) ? value_check : 0;
+int value_less_than_thousand(int value) {
+    return (value < 1000) ? value : 0;
 }
 
-void contains_negative(const char* input, const char* delimiter) {
+int process_input(const char* input, const char* delimiter, int* has_negatives) {
     char* duplicate_input = strdup(input);
+    int sum = 0;
     char* input_segment = strtok(duplicate_input, delimiter);
     while (input_segment != NULL) {
-        int value_check = atoi(input_segment);
-        if (value_check < 0) {
-          throw std::runtime_error("negatives not allowed");
-         }
+        int value = atoi(input_segment);
+        if (value < 0) {
+            *has_negatives = 1;
+        }
+        sum += value_less_than_thousand(value);
         input_segment = strtok(NULL, delimiter);
     }
     free(duplicate_input);
+    return sum;
 }
 
-int calculate_sum(const char* input, const char* delimiter) {
-    int sum = 0;
-    char* input_copy = strdup(input); 
-    char* input_segment = strtok(input_copy, delimiter);
-    while (input_segment != NULL) {
-        sum += value_less_than_thousand(input_segment);
-        input_segment = strtok(NULL, delimiter);
+void negative_value_exception(int has_negatives) {
+    if (has_negatives) {
+        throw std::runtime_error("negatives not allowed");
     }
-    free(input_copy); 
-    return sum;
 }
 
 int add(const char* input) {
@@ -59,6 +55,8 @@ int add(const char* input) {
         return 0;
     }
     has_custom_delimiter(input, delimiter);
-    contains_negative(input, delimiter);
-    return calculate_sum(input, delimiter);
+    int has_negatives = 0;
+    int sum = process_input(input, delimiter, &has_negatives);
+    negative_value_exception(has_negatives);
+    return sum;
 }
