f2964085dab3e5ce131edbd6a7196ff9b9d39ff5
Pavithra121199 <65826287+Pavithra121199@users.noreply.github.com>
2024-07-25 22:42:07 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -27,26 +27,24 @@ int value_less_than_thousand(int value) {
     return (value < 1000) ? value : 0;
 }
 
-int process_input(const char* input, const char* delimiter, int* has_negatives) {
+void negative_value_exception(int value) {
+    if (value<0) {
+        throw std::runtime_error("negatives not allowed");
+    }
+}
+
+int process_input(const char* input, const char* delimiter) {
     char* duplicate_input = strdup(input);
     int sum = 0;
     char* input_segment = strtok(duplicate_input, delimiter);
     while (input_segment != NULL) {
         int value = atoi(input_segment);
-        if (value < 0) {
-            *has_negatives = 1;
-        }
+        check_negative_values(value);
         sum += value_less_than_thousand(value);
         input_segment = strtok(NULL, delimiter);
     }
     free(duplicate_input);
     return sum;
-}
-
-void negative_value_exception(int has_negatives) {
-    if (has_negatives) {
-        throw std::runtime_error("negatives not allowed");
-    }
 }
 
 int add(const char* input) {
@@ -55,8 +53,6 @@ int add(const char* input) {
         return 0;
     }
     has_custom_delimiter(input, delimiter);
-    int has_negatives = 0;
-    int sum = process_input(input, delimiter, &has_negatives);
-    negative_value_exception(has_negatives);
+    int sum = process_input(input, delimiter);
     return sum;
 }
